829f012b2337f31d802597745c6b91dcf94a9663
GeralmyUrena <geralmyuri11@gmail.com>
2023-05-29 10:19:03 -0400

    commit tree 31eb7e9e9b6679c5bdf0ccf8c077c77f9f81cd12

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
+# Define a `name` getter and a `name=` setter method for the instance variable
+# `@name`.
 class Person
-
+  def name=(value)
+    @name = value
+  end
+  def name
+    @name 
+  end
+  def job=(value)
+    @job = value
+  end
+  def job 
+    @job
+  end
 end
